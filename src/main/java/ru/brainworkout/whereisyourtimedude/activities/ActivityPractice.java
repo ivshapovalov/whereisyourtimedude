6d7a3d78f7cd1e209b53581ae85fdd57fdf096b9
Ivan <metaldaze@mail.ru>
2016-09-07 13:00:04 +0300

    GUI: practices done

---
+++ src/main/java/ru/brainworkout/whereisyourtimedude/activities/ActivityPractice.java
@@ -0,0 +1,206 @@
+package ru.brainworkout.whereisyourtimedude.activities;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.brainworkout.whereisyourtimedude.R;
+import ru.brainworkout.whereisyourtimedude.database.entities.Area;
+import ru.brainworkout.whereisyourtimedude.database.entities.Practice;
+import ru.brainworkout.whereisyourtimedude.database.entities.Project;
+import ru.brainworkout.whereisyourtimedude.database.manager.DatabaseManager;
+import ru.brainworkout.whereisyourtimedude.database.manager.TableDoesNotContainElementException;
+
+import static ru.brainworkout.whereisyourtimedude.common.Common.blink;
+import static ru.brainworkout.whereisyourtimedude.common.Common.setTitleOfActivity;
+
+public class ActivityPractice extends AppCompatActivity {
+
+    private Practice mCurrentPractice;
+    private final DatabaseManager DB = new DatabaseManager(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+
+        Intent intent = getIntent();
+        boolean mPracticeIsNew = intent.getBooleanExtra("IsNew", false);
+
+        if (mPracticeIsNew) {
+            mCurrentPractice = new Practice.Builder(DB.getPracticeMaxNumber() + 1).build();
+        } else {
+            int id = intent.getIntExtra("id", 0);
+            try {
+                mCurrentPractice = DB.getPractice(id);
+            } catch (TableDoesNotContainElementException tableDoesNotContainElementException) {
+                tableDoesNotContainElementException.printStackTrace();
+            }
+        }
+        int id_project = intent.getIntExtra("id_project", 0);
+        if (id_project != 0) {
+            mCurrentPractice.setIdProject(id_project);
+        }
+
+        showPracticeOnScreen();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        setTitleOfActivity(this);
+    }
+
+
+    private void showPracticeOnScreen() {
+
+
+        int mIsActiveID = getResources().getIdentifier("cbIsActive", "id", getPackageName());
+        CheckBox cbIsActive = (CheckBox) findViewById(mIsActiveID);
+        if (cbIsActive != null) {
+            if (mCurrentPractice.getIsActive() != 0) {
+                cbIsActive.setChecked(true);
+            } else {
+                cbIsActive.setChecked(false);
+            }
+            cbIsActive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                    if (mCurrentPractice != null) {
+                        if (isChecked) {
+                            mCurrentPractice.setIsActive(1);
+                        } else {
+                            mCurrentPractice.setIsActive(0);
+                        }
+
+                    }
+                }
+            });
+
+        }
+
+
+        //ID
+        int mID = getResources().getIdentifier("tvID", "id", getPackageName());
+        TextView tvID = (TextView) findViewById(mID);
+        if (tvID != null) {
+
+            tvID.setText(String.valueOf(mCurrentPractice.getID()));
+        }
+
+        //Имя
+        int mNameID = getResources().getIdentifier("etName", "id", getPackageName());
+        EditText etName = (EditText) findViewById(mNameID);
+        if (etName != null) {
+            etName.setText(mCurrentPractice.getName());
+        }
+
+        //ID
+        int mProject = getResources().getIdentifier("tvProject", "id", getPackageName());
+        TextView tvProject = (TextView) findViewById(mProject);
+        if (tvProject != null) {
+
+            String nameProject = "";
+            try {
+                Project project = DB.getProject(mCurrentPractice.getIdProject());
+                nameProject = project.getName();
+
+            } catch (TableDoesNotContainElementException e) {
+
+            }
+            tvProject.setText(nameProject);
+        }
+
+    }
+
+    public void btClose_onClick(final View view) {
+
+        blink(view);
+        Intent intent = new Intent(getApplicationContext(), ActivityPracticesList.class);
+        intent.putExtra("id", mCurrentPractice.getID());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+
+
+    private void getPropertiesFromScreen() {
+
+        //Имя
+        int mNameID = getResources().getIdentifier("etName", "id", getPackageName());
+        EditText etName = (EditText) findViewById(mNameID);
+        if (etName != null) {
+
+            mCurrentPractice.setName(String.valueOf(etName.getText()));
+
+        }
+
+    }
+
+    public void tvProject_onClick(View view) {
+
+        blink(view);
+
+        getPropertiesFromScreen();
+
+        mCurrentPractice.dbSave(DB);
+
+        int id_project = mCurrentPractice.getIdProject();
+
+        Intent intent = new Intent(getApplicationContext(), ActivityProjectsList.class);
+        intent.putExtra("id_practice", mCurrentPractice.getID());
+        intent.putExtra("IsNew", false);
+        intent.putExtra("forChoice", true);
+        intent.putExtra("id", id_project);
+        intent.putExtra("CallerActivity", "ActivityPractice");
+        startActivity(intent);
+
+    }
+
+    public void btSave_onClick(final View view) {
+
+        blink(view);
+        getPropertiesFromScreen();
+
+        mCurrentPractice.dbSave(DB);
+
+        Intent intent = new Intent(getApplicationContext(), ActivityPracticesList.class);
+        intent.putExtra("id", mCurrentPractice.getID());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    public void btDelete_onClick(final View view) {
+
+        blink(view);
+
+
+        new AlertDialog.Builder(this)
+                .setMessage("Вы действительно хотите удалить текущее занятие и его историю?")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        DB.deleteAllPracticeHistoryOfPractice(mCurrentPractice.getID());
+
+                        mCurrentPractice.dbDelete(DB);
+
+                        Intent intent = new Intent(getApplicationContext(), ActivityProjectsList.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+                    }
+                }).setNegativeButton("Нет", null).show();
+
+    }
+
+
+}
---
+++ src/main/java/ru/brainworkout/whereisyourtimedude/activities/ActivityPracticesList.java
@@ -0,0 +1,223 @@
+package ru.brainworkout.whereisyourtimedude.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.brainworkout.whereisyourtimedude.R;
+import ru.brainworkout.whereisyourtimedude.common.Common;
+import ru.brainworkout.whereisyourtimedude.database.entities.Area;
+import ru.brainworkout.whereisyourtimedude.database.entities.Practice;
+import ru.brainworkout.whereisyourtimedude.database.entities.Project;
+import ru.brainworkout.whereisyourtimedude.database.manager.AndroidDatabaseManager;
+import ru.brainworkout.whereisyourtimedude.database.manager.DatabaseManager;
+import ru.brainworkout.whereisyourtimedude.database.manager.TableDoesNotContainElementException;
+
+import static ru.brainworkout.whereisyourtimedude.common.Common.HideEditorButton;
+import static ru.brainworkout.whereisyourtimedude.common.Common.blink;
+import static ru.brainworkout.whereisyourtimedude.common.Common.dbCurrentUser;
+import static ru.brainworkout.whereisyourtimedude.common.Common.setTitleOfActivity;
+
+public class ActivityPracticesList extends AppCompatActivity {
+
+    private final int MAX_VERTICAL_BUTTON_COUNT = 17;
+    private final int MAX_HORIZONTAL_BUTTON_COUNT = 2;
+    private final int NUMBER_OF_VIEWS = 40000;
+
+    private final DatabaseManager DB = new DatabaseManager(this);
+
+    private int mHeight = 0;
+    private int mWidth = 0;
+    private int mTextSize = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practices_list);
+
+        if (!Common.isDebug) {
+            int mEditorID = getResources().getIdentifier("btPracticesDBEditor", "id", getPackageName());
+            Button btEditor = (Button) findViewById(mEditorID);
+            HideEditorButton(btEditor);
+        }
+
+        showPractices();
+
+        setTitleOfActivity(this);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        showPractices();
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("id", 0);
+
+        TableRow mRow = (TableRow) findViewById(NUMBER_OF_VIEWS + id);
+        if (mRow != null) {
+            int mScrID = getResources().getIdentifier("svTablePractices", "id", getPackageName());
+            ScrollView mScrollView = (ScrollView) findViewById(mScrID);
+            if (mScrollView != null) {
+
+                mScrollView.requestChildFocus(mRow, mRow);
+            }
+        }
+    }
+
+
+    public void btPracticeAdd_onClick(final View view) {
+
+        blink(view);
+        Intent intent = new Intent(getApplicationContext(), ActivityPractice.class);
+        intent.putExtra("IsNew", true);
+        startActivity(intent);
+
+    }
+
+    private void showPractices() {
+
+        List<Practice> practices;
+        if (dbCurrentUser != null) {
+
+            practices = DB.getAllActivePracticesOfUser(dbCurrentUser.getID());
+        } else {
+            practices = DB.getAllActivePractices();
+        }
+
+        ScrollView sv = (ScrollView) findViewById(R.id.svTablePractices);
+        try {
+
+            sv.removeAllViews();
+
+        } catch (NullPointerException e) {
+        }
+
+        DisplayMetrics displaymetrics = getResources().getDisplayMetrics();
+
+        mHeight = displaymetrics.heightPixels / MAX_VERTICAL_BUTTON_COUNT;
+        mWidth = displaymetrics.widthPixels / MAX_HORIZONTAL_BUTTON_COUNT;
+        mTextSize = (int) (Math.min(mWidth, mHeight) / 1.5 / getApplicationContext().getResources().getDisplayMetrics().density);
+
+        TableRow trowButtons = (TableRow) findViewById(R.id.trowButtons);
+
+        if (trowButtons != null) {
+            trowButtons.setMinimumHeight(mHeight);
+        }
+
+        TableLayout layout = new TableLayout(this);
+        layout.setStretchAllColumns(true);
+
+        for (int numPractice = 0; numPractice < practices.size(); numPractice++) {
+
+            Practice currentPractice = practices.get(numPractice);
+
+            TableRow mRow = new TableRow(this);
+            mRow.setId(NUMBER_OF_VIEWS + currentPractice.getID());
+            mRow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rowPractice_onClick((TableRow) v);
+                }
+            });
+            mRow.setMinimumHeight(mHeight);
+            mRow.setBackgroundResource(R.drawable.bt_border);
+
+            TextView txt = new TextView(this);
+            txt.setText(String.valueOf(currentPractice.getID()));
+            txt.setBackgroundResource(R.drawable.bt_border);
+            txt.setGravity(Gravity.CENTER);
+            txt.setHeight(mHeight);
+            txt.setTextSize(mTextSize);
+            txt.setTextColor(getResources().getColor(R.color.text_color));
+            mRow.addView(txt);
+
+            txt = new TextView(this);
+            String name = String.valueOf(currentPractice.getName());
+            txt.setText(name);
+            txt.setBackgroundResource(R.drawable.bt_border);
+            txt.setGravity(Gravity.CENTER);
+            txt.setHeight(mHeight);
+            txt.setTextSize(mTextSize);
+            txt.setTextColor(getResources().getColor(R.color.text_color));
+            mRow.addView(txt);
+
+            txt = new TextView(this);
+            String nameProject="";
+            try {
+                Project project=DB.getProject(currentPractice.getIdProject());
+                nameProject=project.getName();
+
+            } catch (TableDoesNotContainElementException e) {
+
+            }
+            txt.setText(nameProject);
+            txt.setBackgroundResource(R.drawable.bt_border);
+            txt.setGravity(Gravity.CENTER);
+            txt.setHeight(mHeight);
+            txt.setTextSize(mTextSize);
+            txt.setTextColor(getResources().getColor(R.color.text_color));
+            mRow.addView(txt);
+
+            mRow.setBackgroundResource(R.drawable.bt_border);
+            layout.addView(mRow);
+
+        }
+        sv.addView(layout);
+
+    }
+
+    private void rowPractice_onClick(final TableRow v) {
+
+        blink(v);
+
+        int id = v.getId() % NUMBER_OF_VIEWS;
+
+        Intent intent = new Intent(getApplicationContext(), ActivityPractice.class);
+        intent.putExtra("id", id);
+        intent.putExtra("IsNew", false);
+        startActivity(intent);
+
+    }
+
+    public void btEdit_onClick(final View view) {
+
+        blink(view);
+
+        Intent dbmanager = new Intent(getApplicationContext(), AndroidDatabaseManager.class);
+        startActivity(dbmanager);
+    }
+
+
+    public void buttonHome_onClick(final View view) {
+
+        blink(view);
+        Intent intent = new Intent(getApplicationContext(), ActivityMain.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+
+    public void onBackPressed() {
+
+        Intent intent = new Intent(getApplicationContext(), ActivityMain.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+}
+

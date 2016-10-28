222da5e2c38281257edb14d5a6ca6a305a6f0ffd
Ivan <ivshapovalov@gmail.com>
2016-10-28 11:39:21 +0300

    return all gradle files in git

--- src/main/java/ru/brainworkout/whereisyourtimedude/database/interfaces/DeletingFromDb.java
+++
@@ -1,8 +0,0 @@
-package ru.brainworkout.whereisyourtimedude.database.interfaces;
-
-import ru.brainworkout.whereisyourtimedude.database.manager.DatabaseManager;
-
-public interface DeletingFromDb {
-
-    void dbDelete(DatabaseManager db);
-}
--- src/main/java/ru/brainworkout/whereisyourtimedude/database/manager/TableDoesNotContainElementException.java
+++
@@ -1,9 +0,0 @@
-package ru.brainworkout.whereisyourtimedude.database.manager;
-
-public class TableDoesNotContainElementException extends RuntimeException {
-
-    public TableDoesNotContainElementException(String detailMessage) {
-        super(detailMessage);
-    }
-
-}
